2e5f739f1c63c4f37ec4f6256b87f2a49c968213
siddhikavillisaravanan <143216319+siddhikavillisaravanan@users.noreply.github.com>
2023-08-25 23:53:25 +0530

    Add files via upload
    Week1

---
+++ week1/day1/HelloWorld.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Hello Wordl");
+
+	}
+
+}
